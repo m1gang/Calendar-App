31db274e0945f1c89f1b35671c341c497d0b1a76
MiGaNg <miguelangelyv1@gmail.com>
2025-04-07 20:11:33 -0500

    Creamos la rutas de la aplicación

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom"
+import { AppRouter } from "./router"
+
 
 export const CalendarApp = () => {
-  return (
-    <div>CalendarApp</div>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
+        </>
+    )
 }
